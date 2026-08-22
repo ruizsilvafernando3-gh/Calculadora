c9a3b70b6abaf396d1f1c5ab6f14358658b4a3ab
ruizsilvafernando3-gh <ruizsilvafernando3@gmail.com>
2026-08-22 09:33:25 -0300

    subida de archivo

---
+++ CalculadoraGitHub_22_08.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadoragithub_22_08;
+
+/**
+ *
+ * @author Owner
+ */
+public class CalculadoraGitHub_22_08 {
+
+    public static void main(String[] args) {
+        calculadora c1=new calculadora();
+        c1.setVisible(true);
+    }
+}
